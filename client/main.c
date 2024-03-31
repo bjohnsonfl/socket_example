e32a5678b228a8815bd1fcfb8e821784823f3415
Blake Johnson <blakejohnsonuf@gmail.com>
2024-03-30 22:24:23 -0400

    added poll for send/recieve

--- client/main.c
+++ client/main.c
@@ -5,6 +5,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <poll.h>
+#include <stdlib.h>
+
+#define ONE_SECOND 1000
+#define TIMEOUT 10 * ONE_SECOND
+#define TO_STR(STR) #STR
 
 // input: *sfd: pointer to socket fild descriptor 
 // output: error code, 0 pass, -1 fail
@@ -65,27 +71,86 @@ int comms(int sfd){
     char buff_net [100];
     int matches = 0, len = 0;
     bool quit = false;
-    printf(" Send: \"\\quit\" to break connection\n");
-    while(true){
-        printf("\n > ");
-        char* buff = fgets(buff_loc, 99, stdin);
-        if (buff != NULL){
-            if (strcmp(buff_loc,"\\quit\n") == 0) return 0;
+    int ready = 0;
+    int numOfFiles = 2;
+    int revent = 0;
+    bool err = false;
+    int count = 0;
 
-             printf(" Msg: %s\n", buff_loc);
-             printf(" Number of Bytes: %lu\n", strlen(buff_loc));
-             printf(" Sending...\n");
-             len = send(sfd, buff_loc, strlen(buff_loc), MSG_NOSIGNAL);
-             if (len == -1){
-                printf(" send() failed with rc: %d\n", len);
-                return -1;
-             }
-             else{
-                printf(" send() sent %d bytes\n", len);
-             }
+    printf(" Send: \"\\quit\" to break connection\n");
+
+    //int poll(struct pollfd fds[], nfds_t nfds, int timeout);
+    struct pollfd* pfds;
+    pfds = calloc(numOfFiles, sizeof(struct pollfd));
+
+    // Events for stdin stream. All input from user
+    pfds[0].fd = STDIN_FILENO;
+    pfds[0].events = POLLIN;
+    // Events for socket sfd. All input from socket
+    pfds[1].fd = sfd;
+    pfds[1].events = POLLIN;
+    
+    while(true){
+        printf("\n client [%08d] > ", count++);
+        fflush(stdout);
+        ready = poll(pfds, numOfFiles, TIMEOUT);
+
+        if(ready < 0){
+            printf(" poll() returned with rc: %d", ready);
+            return -1;
+        }
+        else if(ready == 0) printf(" poll() timeout\n");
+        else{
+            for(int i=0; i<numOfFiles; i++){
+                if(pfds[i].revents & (POLLERR | POLLHUP | POLLNVAL)){
+                    printf("\n Revent Error, FD:%d\n", pfds[i].fd);
+                    #define REVENTS_ERR(ERROR) \
+                        if (pfds[i].revents & ERROR) printf(" Error: %s\n", TO_STR(#ERROR))
+                    REVENTS_ERR(POLLERR);
+                    REVENTS_ERR(POLLHUP);
+                    REVENTS_ERR(POLLNVAL);
+                    return -1;
+                }
+                
+                if(pfds[i].fd == STDIN_FILENO){
+                    if(pfds[i].revents && POLLIN){
+                        pfds[i].revents ^= POLLIN;
+                        char* buff = fgets(buff_loc, 99, stdin);
+                        if (buff != NULL){
+                            if (strcmp(buff_loc,"\\quit\n") == 0) return 0;
+
+                             printf(" Msg: %s\n", buff_loc);
+                             printf(" Number of Bytes: %lu\n", strlen(buff_loc));
+                             printf(" Sending...\n");
+                             len = send(sfd, buff_loc, strlen(buff_loc), MSG_NOSIGNAL);
+                             if (len == -1){
+                                printf(" send() failed with rc: %d\n", len);
+                                return -1;
+                             }
+                             else{
+                                printf(" send() sent %d bytes\n", len);
+                             }
+                        }
+                    }
+                }
+                else if(pfds[i].fd == sfd){
+                    if(pfds[i].revents && POLLIN){
+                        pfds[i].revents ^= POLLIN;
+                        len = recv(sfd, buff_net, 100, 0);
+                        printf(" \nMessage recieved...\n");
+                        printf(" Msg len: %d\n", len);
+                        printf(" Msg: %s", buff_net);
+                        bzero(buff_net, len);
+                    }
+                }
+                else{
+                    printf("\n File Descriptor: %d, revent: %x \n", pfds[i].fd, pfds[i].revents);
+                }
+            }
         }
 
     }
+    return 0;
 }
 
 
